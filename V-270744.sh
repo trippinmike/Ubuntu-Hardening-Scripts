e0d69bb2b4413e3b57bfa4739d3477bc411aefaf
N3m0 <106895553+trippinmike@users.noreply.github.com>
2026-01-19 09:43:20 -0800

    Delete V-270744.sh2

--- V-270744.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# ----------------------------------------------------------------------------
-# implement NIST FIPS-validated cryptography to protect classified information 
-# ----------------------------------------------------------------------------
-
-
-#Verify the system is configured to run in FIPS mode
-echo "FIPS not  found on the system. Control failed"
